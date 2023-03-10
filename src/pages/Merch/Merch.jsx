c0b12fef4b2dfbdab8b3ac3dec2e27a668f8b2a2
Kiisi Felix <kiisifelix06@gmail.com>
2023-03-10 15:55:06 +0100

    ci - Added more Merch Images

--- src/pages/Merch/Merch.jsx
+++ src/pages/Merch/Merch.jsx
@@ -6,6 +6,10 @@ import Connect from '../Home/Connect/Connect'
 import cloth_1 from '../../assets/twd_cloth_1.png'
 import cloth_2 from '../../assets/twd_cloth_2.png'
 import cloth_3 from '../../assets/twd_cloth_3.png'
+import cloth_4 from '../../assets/twd_cloth_4.jpeg'
+import cloth_5 from '../../assets/twd_cloth_5.jpeg'
+import cloth_6 from '../../assets/twd_cloth_6.jpeg'
+import cloth_7 from '../../assets/twd_cloth_7.jpeg'
 
 const Merch = () => {
   return (
@@ -19,6 +23,10 @@ const Merch = () => {
                         <img src={cloth_1} alt="The Word Token Merch cloth"/>
                         <img src={cloth_2} alt="The Word Token Merch cloth"/>
                         <img src={cloth_3} alt="The Word Token Merch cloth"/>
+                        <img src={cloth_4} alt="The Word Token Merch cloth"/>
+                        <img src={cloth_5} alt="The Word Token Merch cloth"/>
+                        <img src={cloth_6} alt="The Word Token Merch cloth"/>
+                        <img src={cloth_7} alt="The Word Token Merch cloth"/>
                     </div>
                     <a target="_blank" href="https://instagram.com/mzconglomerate?igshid=YmMyMTA2M2Y=" className={styles.merch__btnWrap}><button className={styles.merch__btn}>Buy Now</button></a>
                     
